ac4e8db0665b227e689c15af1ac01e0b622b564c
Dom Golac <domgolac@gmail.com>
2021-05-02 07:50:32 +0000

    complete lightcoin-crypto activity

--- index.js
+++ index.js
@@ -1,29 +1,83 @@
-let balance = 500.00;
+// let balance = 500.00;
 
-class Withdrawal {
+class Account {
 
-  constructor(amount) {
-    this.amount = amount;
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
   }
 
-  commit() {
-    balance -= this.amount;
+  get balance() {
+    return this.transactions.reduce((acc, val) => {
+      return acc + val.value;
+    }, 0);
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
   }
 
 }
 
+class Transaction {
 
+  constructor(account, amount) {
+    this.amount  = amount;
+    this.account = account;
+  }
 
+  commit() {
+    if (this.isAllowed()) {
+      this.time = new Date();
+      this.account.addTransaction(this);
+      return true;
+    }
+    return false;
+  }
+
+}
+
+class Withdrawal extends Transaction {
+
+  isAllowed() {
+    return this.amount <= this.account.balance;
+  }
+
+  get value() {
+    return -this.amount;
+  }
+
+}
+
+class Deposit extends Transaction {
+
+  isAllowed() {
+    return true;
+  }
+
+  get value() {
+    return this.amount;
+  }
+
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account("snow-patrol");
 
-t1 = new Withdrawal(50.25);
+console.log('Balance:', myAccount.balance);
+t3 = new Deposit(myAccount, 120.00);
+t3.commit();
+console.log('Transaction 3:', t3);
+
+console.log('Balance:', myAccount.balance);
+t1 = new Withdrawal(myAccount, 50.25);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+console.log('Balance:', myAccount.balance);
+t2 = new Withdrawal(myAccount, 9.99);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+console.log('Balance:', myAccount.balance);
